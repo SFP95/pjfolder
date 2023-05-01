329da001cc72cc79ee606cd1111a0938f42bb500
Alex <alejandro@hermeneusworld.com>
2023-05-01 10:58:01 +0200

    ajustes y configuracion de rutas del main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:pjfolder/src/pages/character_creation_page.dart';
+import 'package:pjfolder/src/pages/character_list_page.dart';
+import 'package:pjfolder/src/pages/character_sheet_page.dart';
+import 'package:pjfolder/src/pages/dice_page.dart';
+import 'package:pjfolder/src/pages/gallery_page.dart';
+import 'package:pjfolder/src/pages/login_page.dart';
+import 'package:pjfolder/src/pages/register_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,18 +15,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PjFolder',
+      title: 'RGS - Role Game System',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      routes: {
+        '/':(context)=> LoginPage(),
+        '/register': (context) => RegisterPage(),
+        '/gallery': (context) => GalleryPage(),
+        '/dice': (context) => DicePage(),
+        '/characters': (context) => CharacterListPage(),
+        '/character_sheet': (context) => CharacterSheetPage(),
+        '/character_creation': (context) => CharacterCreationPage(),
+      },
+      initialRoute: '/',
+      /*
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Rol Sheet App'),
+          title: Text('RGS - Role Game System'),
         ),
         body: Center(
-          child: Text('Bienvenido a Rol Sheet App'),
+          child: Text('Bienvenido a Role Game System'),
         ),
-      ),
+      ),*/
+
     );
   }
 }
--- lib/src/pages/character_sheet_page.dart
+++ lib/src/pages/character_sheet_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CharacterSheetListPage extends StatelessWidget {
+class CharacterSheetPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
