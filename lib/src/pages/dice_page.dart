7f2fd09296a06739669b9e33f3755953446b486e
Nia <88581938+NiaD95@users.noreply.github.com>
2023-05-02 00:03:20 +0200

    subida imagenes he icorporacion a pgina dados

--- lib/src/pages/dice_page.dart
+++ lib/src/pages/dice_page.dart
@@ -24,43 +24,106 @@ class _DicePageState extends State<DicePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dados'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            DropdownButton<int>(
-              value: _selectedDice,
-              items: [4, 6, 8, 12, 20].map<DropdownMenuItem<int>>((int value) {
-                return DropdownMenuItem<int>(
-                  value: value,
-                  child: Text('$value caras'),
-                );
-              }).toList(),
-              onChanged: (int? newValue) {
-                setState(() {
-                  _selectedDice = newValue ?? 4;
-                });
-              },
-            ),
-            TextFormField(
-              initialValue: '1',
-              keyboardType: TextInputType.number,
-              onChanged: (String newValue) {
-                setState(() {
-                  _diceCount = int.tryParse(newValue) ?? 1;
-                });
-              },
-            ),
-            ElevatedButton(
-              onPressed: _rollDice,
-              child: Text('Tirar dados'),
-            ),
-            Text('Resultado: $_result'),
-          ],
+    return GestureDetector(
+      child: Scaffold(
+        backgroundColor: Colors.grey[400],
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 0,
+          title: Text('Dados',style: TextStyle(fontSize: 40,color: Colors.grey[400])),
+          backgroundColor: Colors.grey.shade800,
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                  color: Colors.grey[800]
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Nº de caras:",style: TextStyle(fontSize: 30,color: Colors.grey[400])),
+                    Divider(height: 20),
+                    DropdownButton<int>(
+                      dropdownColor: Colors.grey[800],
+                      style: TextStyle(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(30),
+                      value: _selectedDice,
+                      items: [4, 6, 8, 12, 20].map<DropdownMenuItem<int>>((int value) {
+                        return DropdownMenuItem<int>(
+                          value: value,
+                          child: Text('$value caras'),
+                        );
+                      }).toList(),
+                      onChanged: (int? newValue) {
+                        setState(() {
+                          _selectedDice = newValue ?? 4;
+                        });
+                      },
+                    ),
+                ],
+                )
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                    color: Colors.grey[800]),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Nº de Dados:",style: TextStyle(fontSize: 30,color: Colors.grey[400])),
+                    TextFormField(
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hoverColor: Colors.grey,
+                      ),
+                      initialValue: '0',
+                      keyboardType: TextInputType.number,
+                      onChanged: (String newValue) {
+                        setState(() {
+                          _diceCount = int.tryParse(newValue) ?? 1;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                    color: Colors.grey[800]),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade800),
+                      ),
+                      onPressed: _rollDice,
+                      child: Text('Tirar dados',style: TextStyle(fontSize: 30,color: Colors.grey[400])),
+                    ),
+                    Text('Resultado: $_result'),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
