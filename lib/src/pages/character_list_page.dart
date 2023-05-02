8864690dfe6b91f4d373ffe7edf6f9bd0c412106
Nia <88581938+NiaD95@users.noreply.github.com>
2023-05-02 13:17:07 +0200

    ajustes de presentación lista pjs

--- lib/src/pages/character_list_page.dart
+++ lib/src/pages/character_list_page.dart
@@ -6,15 +6,30 @@ class CharacterListPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de Personajes'),
+        backgroundColor: Colors.grey[800],
       ),
       body: ListView.builder(
+        padding: EdgeInsets.all(30),
         itemCount: 10, // Reemplaza este valor con la cantidad de personajes en la lista
         itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text('Personaje $index'), // Reemplaza este texto con el nombre del personaje en la lista
-            onTap: () {
-              Navigator.pushNamed(context, '/character_sheet');
-            },
+          return Container(
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              color: Colors.grey[400],
+                borderRadius:BorderRadius.circular(30) 
+            ),
+            child: ListTile(
+              title: Text('Personaje $index',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.grey[800],
+                    fontSize: 20,
+                  )), // Reemplaza este texto con el nombre del personaje en la lista
+              onTap: () {
+                Navigator.pushNamed(context, '/character_sheet');
+              },
+            ),
           );
         },
       ),
