02af86ff9108b9a5336a7d7baca0285deb218686
Nia <88581938+NiaD95@users.noreply.github.com>
2023-05-02 20:44:58 +0200

    ajustes de presentación creacion personaje,pero falta editar componentes

--- lib/src/pages/character_creation_page.dart
+++ lib/src/pages/character_creation_page.dart
@@ -17,15 +17,163 @@ class _CharacterCreationPageState extends State<CharacterCreationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[800],
       appBar: AppBar(
-        title: Text('Creación de Personaje'),
+        iconTheme: IconThemeData(
+            color: Colors.grey[400]
+        ),
+        centerTitle: true,
+        elevation: 0,
+        title: Text('Creación de Personaje',style: TextStyle(fontSize: 25,color: Colors.grey[400])),
+        backgroundColor: Colors.grey.shade800,
       ),
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            // Coloca aquí los widgets personalizados para crear el personaje
-            // Por ejemplo: RaceDropdown(), StatContainer(), StoryContainer(), PhysicalCharacteristicsContainer()
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: Colors.grey[400]
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CircleAvatar(
+                      maxRadius: 80,
+                      backgroundColor: Colors.grey[800],
+                      child: Image.asset("assets/images/perfil.png",color: Colors.grey[400],) //la ruta de ima imagen que debe se cogida de la galeria
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text("NOMBRE",style: TextStyle(fontSize: 30,color: Colors.grey[800])),
+                      Text("APELLIDO",style: TextStyle(fontSize: 25,color: Colors.grey[800])),
+                      Text("RAZA",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                      Text("EDAD",style: TextStyle(fontSize: 15,color: Colors.grey[800])),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(40,40,40,40),
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.grey[400],
+              ),
+              child: Column(
+                children: [
+                  Text("STATS",style: TextStyle(fontSize: 25,color: Colors.grey[800])),
+                  Divider(color: Colors.grey[400],height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Velocidad :",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                          Text("Fuerza :",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                          Text("Agilidad :",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                          Text("Inteligencia :",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(20),
+                            padding: EdgeInsets.all(10),
+                            color: Colors.white60,
+                            child:
+                            Column(
+                              children: [
+                                Text("0",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                                Text("0",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                                Text("0",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                                Text("0",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(30),
+              padding: EdgeInsets.all(1),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.grey[400]
+              ),
+              child: Column(
+                children: [
+                  Text("Historia",style: TextStyle(fontSize: 25,color: Colors.grey[800])),
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    padding: EdgeInsets.all(60),
+                    color: Colors.white60,
+                    child: Text("CONTENIDO DE LA HISTORIA"),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(40,40,40,40),
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.grey[400]
+              ),
+              child: Column(
+                children: [
+                  Text("Caracteristicas",style: TextStyle(fontSize: 25,color: Colors.grey[800])),
+                  Divider(color: Colors.grey[400],height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Pelo :",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                          Text("Piel :",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                          Text("Rascos :",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                          Text("Vestimenta :",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                          Text("Caracter :",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                          Text("Mascota :",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(20),
+                            padding: EdgeInsets.all(10),
+                            color: Colors.white60,
+                            child:
+                            Column(
+                              children: [
+                                Text("--",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                                Text("--",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                                Text("--",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                                Text("--",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                                Text("--",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                                Text("--",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
