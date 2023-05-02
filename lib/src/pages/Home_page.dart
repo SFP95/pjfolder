2b47fe3bc313cfef12c24494fbbd2ca789d5c14e
Nia <88581938+NiaD95@users.noreply.github.com>
2023-05-02 11:09:57 +0200

    ajustes de presentación

--- lib/src/pages/Home_page.dart
+++ lib/src/pages/Home_page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         elevation: 0,
-        title: Text('Home',style: TextStyle(fontSize: 40,color: Colors.grey[400])),
+        //title: Text('Home',style: TextStyle(fontSize: 40,color: Colors.grey[400])),
         backgroundColor: Colors.grey.shade800,
       ),
       body: Center(
@@ -18,66 +18,80 @@ class HomePage extends StatelessWidget {
           crossAxisSpacing: 30.3,
           crossAxisCount: 2,
           children: [
-            IconButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade400),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(80), // Borde redondeado
-                  ),
-                ),
-              ),
-                onPressed: (){
-                  Navigator.pushNamed(context, '/dice');
-                },
-                icon: Image.asset('assets/images/dado.png',color:Colors.grey.shade400, height: 140, ),
-            ),
-            IconButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade400),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(80), // Borde redondeado
+              Tooltip(
+                message: "DADOS",
+                child: IconButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade400),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(80), // Borde redondeado
+                        ),
+                      ),
                     ),
+                      onPressed: (){
+                        Navigator.pushNamed(context, '/dice');
+                      },
+                      icon: Image.asset('assets/images/dado.png',color:Colors.grey.shade400, height: 140,  ),
                   ),
-                ),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/characters_list');
-                },
-                icon: Image.asset('assets/images/nota.png',color:Colors.grey.shade400, height: 140)
-            ),
-            IconButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade400),
-                //elevation: 0,
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(80), // Borde redondeado
-                  ),
-                ),
               ),
-              onPressed: () {
-                Navigator.pushNamed(context, '/gallery');
-              },
-              icon: Image.asset('assets/images/galeria.png',color:Colors.grey.shade400, height: 140),
-            ),
-            IconButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade400),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(80), // Borde redondeado
+                Tooltip(
+                  message: "Lista de Personajes",
+                  child: IconButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade400),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(80), // Borde redondeado
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/characters_list');
+                      },
+                      icon: Image.asset('assets/images/nota.png',color:Colors.grey.shade400, height: 140)
                   ),
                 ),
-              ),
-              onPressed: () {
-                Navigator.pushNamed(context, '/login');
-              },
-              icon: Image.asset('assets/images/perfil.png',color:Colors.grey.shade400, height: 140),
-            ),
+
+                Tooltip(
+                  message: "Galeria",
+                  child: IconButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade400),
+                      //elevation: 0,
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(80), // Borde redondeado
+                        ),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/gallery');
+                    },
+                    icon: Image.asset('assets/images/galeria.png',color:Colors.grey.shade400, height: 140),
+                  ),
+                ),
+                Tooltip(
+                  message: "Log / Sing In",
+                  child: IconButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade400),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(80), // Borde redondeado
+                        ),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/login');
+                    },
+                    icon: Image.asset('assets/images/perfil.png',color:Colors.grey.shade400, height: 140),
+                  ),
+                ),
+
           ],
         ),
-      ),
+      )
     );
   }
 }
