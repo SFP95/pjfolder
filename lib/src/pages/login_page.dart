67b6fdad1aeb818a40d2bdea96ddede60c681bdf
Nia <88581938+NiaD95@users.noreply.github.com>
2023-05-10 21:47:02 +0200

    ajustes visuales login y register

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -19,34 +19,65 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
+      backgroundColor: Colors.grey[800],
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextFormField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                  color: Colors.grey[400],
+                  borderRadius:BorderRadius.circular(30)
+              ),
+              child: Column(
+                children: [
+                  TextFormField(
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey[400],
+                    ),
+                    controller: _emailController,
+                    decoration: InputDecoration(labelText: 'Email'),
+                  ),
+                  TextFormField(
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey[400],
+                    ),
+                    controller: _passwordController,
+                    decoration: InputDecoration(labelText: 'Contraseña'),
+                    obscureText: true,
+                  ),
+                ],
+              ),
             ),
-            TextFormField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Contraseña'),
-              obscureText: true,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly ,
+              children: [
+                ElevatedButton(
+                  style:  ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade400),
+                   ),
+                  onPressed: () {
+                    // Implementar la lógica de inicio de sesión aquí
+                  },
+                  child: Text('Iniciar sesión', style: TextStyle(color: Colors.grey[800],fontSize: 20)),
+                ),
+                ElevatedButton(
+                  style:  ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade400),
+                  ),
+                  onPressed: () {
+                    Navigator.popAndPushNamed(context, '/register');
+                  },
+                  child: Text('Registrarse', style: TextStyle(color: Colors.grey[800],fontSize: 20)),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                // Implementar la lógica de inicio de sesión aquí
-              },
-              child: Text('Iniciar sesión'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/register');
-              },
-              child: Text('Registrarse'),
-            ),
+
           ],
         ),
       ),
--- lib/src/pages/register_page.dart
+++ lib/src/pages/register_page.dart
@@ -21,31 +21,59 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Registrarse'),
-      ),
+      backgroundColor: Colors.grey[800],
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextFormField(
-              controller: _usernameController,
-              decoration: InputDecoration(labelText: 'Nombre de usuario'),
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                  color: Colors.grey[400],
+                  borderRadius:BorderRadius.circular(30)
+              ),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(labelText: 'Nombre de usuario'),
+                  ),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(labelText: 'Email'),
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(labelText: 'Contraseña'),
+                    obscureText: true,
+                  ),
+                ],
+              ),
             ),
-            TextFormField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            TextFormField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Contraseña'),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Implementar la lógica de registro aquí
-              },
-              child: Text('Registrarse'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly ,
+              children: [
+                ElevatedButton(
+                  style:  ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade400),
+                  ),
+                  onPressed: () {
+                    // Implementar la lógica de registro aquí
+                  },
+                  child: Text('Registrarse', style: TextStyle(color: Colors.grey[800],fontSize: 20)),
+                ),
+                ElevatedButton(
+                  style:  ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade400),
+                  ),
+                  onPressed: () {
+                    Navigator.popAndPushNamed(context, '/home');
+                  },
+                  child: Text('Cancelar', style: TextStyle(color: Colors.grey[800],fontSize: 20)),
+                ),
+              ],
             ),
           ],
         ),
