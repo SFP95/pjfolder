f1869861e4dc4ba51d17c398dd2cc607f39aa59d
Alex <alejandro@hermeneusworld.com>
2023-05-21 10:04:17 +0200

    aajuste de ruta perfil usuario corrup, se cambia al login por el momento

--- lib/src/pages/Home_page.dart
+++ lib/src/pages/Home_page.dart
@@ -83,7 +83,7 @@ class HomePage extends StatelessWidget {
                       ),
                     ),
                     onPressed: () {
-                      Navigator.pushNamed(context, '/profile_UserLog');
+                      Navigator.pushNamed(context, '/login');
                     },
                     icon: Image.asset('assets/images/perfil.png',color:Colors.grey.shade400, height: 140),
                   ),
