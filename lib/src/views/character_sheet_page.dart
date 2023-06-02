94cabdd61525e83ad7ad68f7f0396d4bc246f77a
Nia <88581938+NiaD95@users.noreply.github.com>
2023-06-02 22:26:37 +0200

    ajustes visuales

--- lib/src/views/character_sheet_page.dart
+++ lib/src/views/character_sheet_page.dart
@@ -1,17 +1,54 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CharacterSheetPage extends StatelessWidget {
+class CharacterSheetPage extends StatefulWidget {
+  @override
+  _CharacterSheetPageState createState() => _CharacterSheetPageState();
+}
+
+class _CharacterSheetPageState extends State<CharacterSheetPage> {
+  int _speedValue = 0;
+  int _forceValue = 0;
+  int _agilityValue = 0;
+  int _intelligenceValue = 0;
+
+  void incrementValue(String stat) {
+    setState(() {
+      if (stat == 'speed') {
+        _speedValue++;
+      } else if (stat == 'force') {
+        _forceValue++;
+      } else if (stat == 'agility') {
+        _agilityValue++;
+      } else if (stat == 'intelligence') {
+        _intelligenceValue++;
+      }
+    });
+  }
+
+  void decrementValue(String stat) {
+    setState(() {
+      if (stat == 'speed') {
+        _speedValue--;
+      } else if (stat == 'force') {
+        _forceValue--;
+      } else if (stat == 'agility') {
+        _agilityValue--;
+      } else if (stat == 'intelligence') {
+        _intelligenceValue--;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[800],
       appBar: AppBar(
-        iconTheme: IconThemeData(
-            color: Colors.grey[400]
-        ),
+        iconTheme: IconThemeData(color: Colors.grey[400]),
         centerTitle: true,
         elevation: 0,
-        title: Text('Character File',style: TextStyle(fontSize: 30,color: Colors.grey[400])),
+        title: Text('Character File', style: TextStyle(fontSize: 30, color: Colors.grey[400])),
         backgroundColor: Colors.grey.shade800,
       ),
       body: SingleChildScrollView(
@@ -22,7 +59,7 @@ class CharacterSheetPage extends StatelessWidget {
             Container(
               padding: EdgeInsets.all(10),
               decoration: BoxDecoration(
-                color: Colors.grey[400]
+                color: Colors.grey[400],
               ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -30,86 +67,177 @@ class CharacterSheetPage extends StatelessWidget {
                   CircleAvatar(
                     maxRadius: 80,
                     backgroundColor: Colors.grey[800],
-                    child: Image.asset("assets/images/perfil.png",color: Colors.grey[400],) //la ruta de ima imagen que debe se cogida de la galeria
+                    child: Image.asset(
+                      "assets/images/perfil.png",
+                      color: Colors.grey[400],
+                    ),
                   ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Text("NAME",style: TextStyle(fontSize: 30,color: Colors.grey[800])),
-                      Text("LAST NAME",style: TextStyle(fontSize: 25,color: Colors.grey[800])),
-                      Text("AGE",style: TextStyle(fontSize: 25,color: Colors.grey[800])),
-                      Text("RACE",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                      Text("NAME", style: TextStyle(fontSize: 30, color: Colors.grey[800])),
+                      Text("LAST NAME", style: TextStyle(fontSize: 25, color: Colors.grey[800])),
+                      Text("AGE", style: TextStyle(fontSize: 25, color: Colors.grey[800])),
+                      Text("RACE", style: TextStyle(fontSize: 20, color: Colors.grey[800])),
                     ],
                   ),
                 ],
               ),
             ),
             Container(
-              margin: EdgeInsets.fromLTRB(40,40,40,40),
+              margin: EdgeInsets.fromLTRB(40, 40, 40, 40),
               padding: EdgeInsets.all(20),
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Colors.grey[400],
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.grey[400],
               ),
               child: Column(
                 children: [
-                  Text("STATS",style: TextStyle(fontSize: 25,color: Colors.grey[800])),
-                  Divider(color: Colors.grey[400],height: 20,),
+                  Text("STATS", style: TextStyle(fontSize: 25, color: Colors.grey[800])),
+                  Divider(color: Colors.grey[400], height: 20,),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text("Speed :",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
-                          Text("Force :",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
-                          Text("Agility :",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
-                          Text("Intelligence :",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                          Text("Speed :", style: TextStyle(fontSize: 20, color: Colors.grey[800])),
+                          Divider(height: 25),
+                          Text("Force :", style: TextStyle(fontSize: 20, color: Colors.grey[800])),
+                          Divider(height: 25),
+                          Text("Agility :", style: TextStyle(fontSize: 20, color: Colors.grey[800])),
+                          Divider(height: 25),
+                          Text("Intelligence :", style: TextStyle(fontSize: 20, color: Colors.grey[800])),
                         ],
                       ),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Container(
-                            margin: EdgeInsets.all(20),
-                            padding: EdgeInsets.all(10),
-
-                            child:
-                            Column(
+                            child: Column(
                               children: [
-                                Text("0",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
-                                Text("0",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
-                                Text("0",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
-                                Text("0",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
+                                Row(
+                                  children: [
+                                    FloatingActionButton(
+                                      onPressed: () => decrementValue('speed'),
+                                      child: Icon(Icons.remove),
+                                      backgroundColor: Colors.grey[700],
+                                      foregroundColor: Colors.grey[400],
+                                      mini: true,
+                                    ),
+                                    Text("   $_speedValue   ", style: TextStyle(fontSize: 25, color: Colors.grey[800])),
+                                    FloatingActionButton(
+                                      onPressed: () => incrementValue('speed'),
+                                      child: Icon(Icons.add),
+                                      backgroundColor: Colors.grey[700],
+                                      foregroundColor: Colors.grey[400],
+                                      mini: true,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    FloatingActionButton(
+                                      onPressed: () => decrementValue('force'),
+                                      child: Icon(Icons.remove),
+                                      backgroundColor: Colors.grey[700],
+                                      foregroundColor: Colors.grey[400],
+                                      mini: true,
+                                    ),
+                                    Text("   $_forceValue   ", style: TextStyle(fontSize: 25, color: Colors.grey[800])),
+                                    FloatingActionButton(
+                                      onPressed: () => incrementValue('force'),
+                                      child: Icon(Icons.add),
+                                      backgroundColor: Colors.grey[700],
+                                      foregroundColor: Colors.grey[400],
+                                      mini: true,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    FloatingActionButton(
+                                      onPressed: () => decrementValue('agility'),
+                                      child: Icon(Icons.remove),
+                                      backgroundColor: Colors.grey[700],
+                                      foregroundColor: Colors.grey[400],
+                                      mini: true,
+                                    ),
+                                    Text("   $_agilityValue   ", style: TextStyle(fontSize: 25, color: Colors.grey[800])),
+                                    FloatingActionButton(
+                                      onPressed: () => incrementValue('agility'),
+                                      child: Icon(Icons.add),
+                                      backgroundColor: Colors.grey[700],
+                                      foregroundColor: Colors.grey[400],
+                                      mini: true,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    FloatingActionButton(
+                                      onPressed: () => decrementValue('intelligence'),
+                                      child: Icon(Icons.remove),
+                                      backgroundColor: Colors.grey[700],
+                                      foregroundColor: Colors.grey[400],
+                                      mini: true,
+                                    ),
+                                    Text("   $_intelligenceValue   ", style: TextStyle(fontSize: 25, color: Colors.grey[800])),
+                                    FloatingActionButton(
+                                      onPressed: () => incrementValue('intelligence'),
+                                      child: Icon(Icons.add),
+                                      backgroundColor: Colors.grey[700],
+                                      foregroundColor: Colors.grey[400],
+                                      mini: true,
+                                    ),
+                                  ],
+                                ),
                               ],
                             ),
                           ),
                         ],
                       ),
                     ],
-                  )
+                  ),
                 ],
               ),
             ),
             Container(
-              margin: EdgeInsets.fromLTRB(40,40,40,40),
+              margin: EdgeInsets.fromLTRB(40, 40, 40, 40),
               padding: EdgeInsets.all(10),
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Colors.grey[400]
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.grey[400],
               ),
               child: Column(
                 children: [
-                  Text("Characteristics",style: TextStyle(fontSize: 25,color: Colors.grey[800])),
-                  Divider(color: Colors.grey[400],height: 20,),
+                  Text("Characteristics", style: TextStyle(fontSize: 25, color: Colors.grey[800])),
+                  Divider(color: Colors.grey[400], height: 20,),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text("......\n",style: TextStyle(fontSize: 20,color: Colors.grey[800])),
-
+                          Text("......\n", style: TextStyle(fontSize: 20, color: Colors.grey[800])),
                         ],
                       ),
                     ],
@@ -118,15 +246,15 @@ class CharacterSheetPage extends StatelessWidget {
               ),
             ),
             Container(
-              margin: EdgeInsets.fromLTRB(40,40,40,40),
+              margin: EdgeInsets.fromLTRB(40, 40, 40, 40),
               padding: EdgeInsets.all(10),
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Colors.grey[400]
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.grey[400],
               ),
               child: Column(
                 children: [
-                  Text("History",style: TextStyle(fontSize: 25,color: Colors.grey[800])),
+                  Text("History", style: TextStyle(fontSize: 25, color: Colors.grey[800])),
                   Container(
                     margin: EdgeInsets.all(20),
                     padding: EdgeInsets.all(60),
@@ -146,7 +274,7 @@ class CharacterSheetPage extends StatelessWidget {
             onPressed: () {
               Navigator.pushNamed(context, '/character_edit');
             },
-            child: Icon(Icons.edit,size: 30,color: Colors.grey[800],),
+            child: Icon(Icons.edit, size: 30, color: Colors.grey[800],),
           ),
           SizedBox(height: 10),
           FloatingActionButton(
@@ -154,7 +282,7 @@ class CharacterSheetPage extends StatelessWidget {
             onPressed: () {
               // Implementar la lógica de borrado aquí
             },
-            child: Icon(Icons.delete,size: 30,color: Colors.grey[800],),
+            child: Icon(Icons.delete, size: 30, color: Colors.grey[800],),
           ),
         ],
       ),
