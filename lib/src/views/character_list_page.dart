a5eb5330e8e1c3d44bbe954c7fb46178a8897fdd
Nia <88581938+NiaD95@users.noreply.github.com>
2023-06-13 21:38:12 +0200

    ajustes con bb a la characters

--- lib/src/views/character_list_page.dart
+++ lib/src/views/character_list_page.dart
@@ -1,6 +1,48 @@
+import 'package:RGS/src/models/character/Character.dart';
+import 'package:RGS/src/utils/http_utils.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class CharacterListPage extends StatelessWidget {
+class CharacterListPage extends StatefulWidget {
+  @override
+  _CharacterListPageState createState() => _CharacterListPageState();
+}
+
+class _CharacterListPageState extends State<CharacterListPage> {
+  List<Character> characters = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCharacters();
+  }
+
+  Future<void> fetchCharacters() async {
+    try {
+      // URL del endpoint de la API para obtener la lista de personajes
+      String endpoint = 'characters';
+
+      // Realiza una solicitud GET al servidor para obtener la lista de personajes
+      http.Response response = await ApiClient().get(endpoint);
+
+      if (response.statusCode == 200) {
+        // Decodifica la respuesta JSON y crea instancias de Character
+        List<dynamic> data = jsonDecode(response.body);
+        List<Character> fetchedCharacters = data.map((item) => Character.fromJson(item)).toList();
+
+        setState(() {
+          characters = fetchedCharacters;
+        });
+      } else {
+        print('Error al obtener la lista de personajes. Código de estado: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error al obtener la lista de personajes: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,34 +53,49 @@ class CharacterListPage extends StatelessWidget {
           size: 30,
         ),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_rounded), // Icono a mostrar
+          icon: Icon(Icons.arrow_back_ios_rounded),
           onPressed: () {
             Navigator.popAndPushNamed(context, '/home');
           },
         ),
         centerTitle: true,
         elevation: 0,
-        title: Text('List of Characters',style: TextStyle(fontSize: 30,color: Colors.grey[400])),
+        title: Text(
+          'List of Characters',
+          style: TextStyle(fontSize: 30, color: Colors.grey[400]),
+        ),
         backgroundColor: Colors.grey.shade800,
       ),
       body: ListView.builder(
         padding: EdgeInsets.all(30),
-        itemCount: 10, // Reemplaza este valor con la cantidad de personajes en la lista
+        itemCount: characters.length,
         itemBuilder: (BuildContext context, int index) {
+          Character character = characters[index];
+
           return Container(
             margin: EdgeInsets.all(10),
             padding: EdgeInsets.all(30),
             decoration: BoxDecoration(
               color: Colors.grey[400],
-                borderRadius:BorderRadius.circular(30) 
+              borderRadius: BorderRadius.circular(30),
             ),
             child: ListTile(
-              title: Text('Character $index',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.grey[800],
-                    fontSize: 30,
-                  )), // Reemplaza este texto con el nombre del personaje en la lista
+              title: Text(
+                character.name,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.grey[800],
+                  fontSize: 30,
+                ),
+              ),
+              subtitle: Text(
+                character.race,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.grey[600],
+                  fontSize: 20,
+                ),
+              ),
               onTap: () {
                 Navigator.pushNamed(context, '/character_sheet');
               },
